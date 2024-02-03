debbe9961a3a64f37c7ed37a8fe4a0ce3a9ad13f
chubinkk <ksb8183@gmail.com>
2024-02-03 15:33:37 +0900

    inital commit

---
+++ programmers/firstNstring/firstNstring.js
@@ -0,0 +1,13 @@
+// function solution(my_string, n) {
+//     var answer = '';
+//     answer = my_string[n-1];
+//     console.log(answer);
+//     return answer;
+// }
+
+// solution("abcdefg", 8)
+
+a = "abnklda29213"
+console.log(typeof(a));
+
+console.log(a.substr(0,5));
